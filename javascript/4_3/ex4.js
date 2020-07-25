2e760be6831a37803508bbe9acb4324615bdb18c
lucasvr39 <lucasveras1994@gmail.com>
2020-07-25 17:20:47 -0300

    exercise 4 - prints a pyramid made of n stars of base

---
+++ javascript/4_3/ex4.js
@@ -0,0 +1,13 @@
+let n = 20;
+let starIncrement = "*";
+let spaceIncrement = "";
+
+for (let i = n; i >= 1; i -= 2) {
+  for (let j = 1; j <= i; j += 2) {
+    spaceIncrement += " ";
+  }
+
+  console.log(spaceIncrement + starIncrement)
+  spaceIncrement = "";
+  starIncrement += "**";
+}
