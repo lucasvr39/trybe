1d73266b0af824d99f0acbc1358436b067ed8ea7
lucasvr39 <lucasveras1994@gmail.com>
2020-07-25 16:14:41 -0300

    exercise 7 - find out the lower value in the array and print it

---
+++ javascript/4_2/ex7.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let lower = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+  for (let j = 0; j < i; j += 1) {
+    if (numbers[i] > numbers[j]) {
+      lower = numbers[j];
+    }
+  }
+}
+
+console.log(lower);
