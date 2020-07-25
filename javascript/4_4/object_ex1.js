21fa26d34aa6956ada6d106bd4f2f82ebdd2bb73
lucasvr39 <lucasveras1994@gmail.com>
2020-07-25 17:21:33 -0300

    exercise 1 - print a welcome message on the console to the character

---
+++ javascript/4_4/object_ex1.js
@@ -0,0 +1,7 @@
+var info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+console.log("Welcome, " + info.personagem);
