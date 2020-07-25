6dc32376d3c5f2c0db51950b20065a5043e86bbc
lucasvr39 <lucasveras1994@gmail.com>
2020-07-25 17:23:35 -0300

    exercise 4 - make a for / in that shows all the values of the object

---
+++ javascript/4_4/object_ex4.js
@@ -0,0 +1,11 @@
+var info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+info.recorrente = "Sim";
+
+for (keys in info) {
+  console.log(info[keys]);
+}
