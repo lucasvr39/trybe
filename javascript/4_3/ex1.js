dc875d9dd0e4e9895a85d4ba0bca27281dc5b9a7
lucasvr39 <lucasveras1994@gmail.com>
2020-07-25 16:16:35 -0300

    exercise 1 - prints a square of side equal n made by stars

---
+++ javascript/4_3/ex1.js
@@ -0,0 +1,10 @@
+let n = 5;
+let increment = "*";
+
+for (let i = 1; i <= n; i += 1) {
+  for (let j = n; j != 1; j -= 1) {
+    increment += "*";
+  }
+  console.log(increment);
+  increment = "*";
+}
